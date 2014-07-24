90ac77c20a58fe301ef8d8bf6df2739aab22259b
Joe Moore <joe.moore@siemens.com>
2014-07-24 15:47:57 -0400

    Working with new suricata-2.0.2

--- suri/sensor4-setup.sh
+++ suri/sensor4-setup.sh
@@ -4,7 +4,7 @@
 DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND
 
-apt-add-repository ppa:oisf/suricata-stable
+apt-add-repository -y ppa:oisf/suricata-stable
 apt-get update
 apt-get -y install suricata securityonion-barnyard2 oinkmaster
 
@@ -12,16 +12,19 @@ mkdir /var/log/nsm /var/log/barnyard2
 ln -s /etc/suricata/rules /etc/nsm/rules
 ln -s /etc/suricata /etc/snort
 
+cp /vagrant/local.rules /etc/suricata/rules
+
 # Generate /etc/snort/sid-msg.map
-/usr/share/oinkmaster/create-sidmap.pl /etc/nsm/rules > /etc/snort/sid-msg.map
+/usr/share/oinkmaster/create-sidmap.pl /etc/suricata/rules > /etc/suricata/sid-msg.map
+touch /etc/snort/gen-msg.map
 
 # Run barnyard2
-#( barnyard2 -c /vagrant/barnyard2.conf -v -f unified2.alert -d /var/log/nsm -i eth1 -h sensor3 2>&1 | tee -a /var/log/barnyard2.out | logger -t barnyard -i --server 192.168.33.9 -P 8514 -u /dev/null ) &
+( barnyard2 -c /vagrant/barnyard2.conf -v -f unified2.alert -d /var/log/nsm -i eth1 -h sensor4 2>&1 | tee -a /var/log/barnyard2.out | logger -t barnyard -i --server 192.168.33.9 -P 8514 -u /dev/null ) &
 
-#( suricata -c /vagrant/suricata.yaml -i eth1 2>&1 | tee -a /var/log/suricata.out | logger -t suricata -i --server 192.168.33.9 -P 8514 -u /dev/null ) &
+( suricata -c /vagrant/suricata.yaml -i eth1 2>&1 | tee -a /var/log/suricata.out | logger -t suricata -i --server 192.168.33.9 -P 8514 -u /dev/null ) &
 
-#sleep 5
+sleep 5
 
 # Add some test data
-#ping -s 1400 -c 5 192.168.33.1
+ping -s 1400 -c 5 192.168.33.1
 
