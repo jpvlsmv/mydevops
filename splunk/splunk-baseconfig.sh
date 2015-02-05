606808d6cb4c69b200c8e9706b6ff6fe519edd20
Joe Moore <joe.moore@siemens.com>
2015-02-05 09:15:06 -0500

    New app and splunk versions

--- splunk/splunk-baseconfig.sh
+++ splunk/splunk-baseconfig.sh
@@ -10,9 +10,10 @@ cat >> /opt/splunk/etc/system/local/server.conf <<EOF
 active_group = Free
 EOF
 
-$SPLUNK install app /vagrant/sw/sideview-utils-lgpl_135.tgz
+$SPLUNK install app /vagrant/sw/sideview-utils.tar.gz
 $SPLUNK install app /vagrant/sw/sos-splunk-on-splunk_32.tgz
 $SPLUNK install app /vagrant/sw/cef-common-event-format-extraction-utilities-123.tgz
+$SPLUNK install app /vagrant/sw/splunk-add-on-for-amazon-web-services_101.tgz
 ln -s /vagrant/gcst_vagrant /opt/splunk/etc/apps/gcst_vagrant
 
 $SPLUNK restart splunkd
